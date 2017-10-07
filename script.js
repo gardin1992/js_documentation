3c4e73e25f059f9cf185ac9e3db0f593d5a77d14
Joao Antonio Gardin Vieira <gardin1992@gmail.com>
2017-10-07 03:24:23 -0300

    First Teste

---
+++ script.js
@@ -0,0 +1,15 @@
+/**
+ * @author Joao Antonio <gardin1992@gmail.com>
+ */
+function Main_Controller () {
+
+	/**
+	 * Metodo public interno a classe
+	 */
+	this.innerMethod = function () {}
+
+	// Metodo interno comentario uma linha
+	// segunda linha
+	function _privateMethods() {}
+
+}
